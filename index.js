636fe3ea64fcb5ddedd61d64e5e5b9b5185a2cc6
Ganeshkanchimad <86827594+Ganeshkanchimad@users.noreply.github.com>
2021-09-20 19:48:10 +0530

    Final commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const totalBillAmount = document.querySelector("#total-bill-amount");
+const totalCashGiven = document.querySelector("#total-cash-given");
+const checkButton = document.querySelector("#check");
+const errorDisplay = document.querySelector("#error-display");
+const errorMessage1 = "Total Bill Amount must be greater than 0.";
+const errorMessage2 = "No change needs to be returned.";
+
+const tables = [document.querySelector("#table-1"), document.querySelector("#table-2"), document.querySelector("#table-3"), document.querySelector("#table-4"), document.querySelector("#table-5"), document.querySelector("#table-6"), document.querySelector("#table-7"), document.querySelector("#table-8"), document.querySelector("#table-9")]
+
+
+const notes = ["2000", "500", "200", "100", "50", "20", "10", "5", "1"];
+
+checkButton.addEventListener("click", function verifyTotalBillAndCashAmount() {
+    var returningAmount = (totalCashGiven.value - totalBillAmount.value);
+    errorDisplay.style.display = "none";
+    if (totalBillAmount.value > 0) {
+        if (totalCashGiven.value >= totalBillAmount.value) {
+            for (i = 0; i < 9; i++) {
+
+                var noOfNotes = (Math.trunc(returningAmount / notes[i]));
+                tables[i].innerText = noOfNotes;
+                returningAmount = (returningAmount) - (noOfNotes * notes[i]);
+            }
+
+        } else {
+            showMessage(errorMessage2)
+        }
+    } else {
+        showMessage(errorMessage1)
+    }
+});
+
+function showMessage(message) {
+    errorDisplay.style.display = "block";
+    errorDisplay.innerText = message;
+}
