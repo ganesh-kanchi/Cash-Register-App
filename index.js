479b6e2b4b6767ccb4a1e195fa77a9ded692b3fe
Ganeshkanchimad <86827594+Ganeshkanchimad@users.noreply.github.com>
2021-09-21 22:10:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ const errorMessage2 = "No change needs to be returned.";
 const tables = [document.querySelector("#table-1"), document.querySelector("#table-2"), document.querySelector("#table-3"), document.querySelector("#table-4"), document.querySelector("#table-5"), document.querySelector("#table-6"), document.querySelector("#table-7"), document.querySelector("#table-8"), document.querySelector("#table-9")]
 
 
-const notes = ["2000", "500", "200", "100", "50", "20", "10", "5", "1"];
+const notes = ["2000", "500", "100", "20", "10", "5", "1"];
 
 checkButton.addEventListener("click", function verifyTotalBillAndCashAmount() {
     var returningAmount = (totalCashGiven.value - totalBillAmount.value);
     errorDisplay.style.display = "none";
     if (totalBillAmount.value > 0) {
         if (+totalCashGiven.value >= +totalBillAmount.value) {
-            for (i = 0; i < 9; i++) {
+            for (i = 0; i < 7; i++) {
 
                 var noOfNotes = (Math.trunc(returningAmount / notes[i]));
                 tables[i].innerText = noOfNotes;
