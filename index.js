637429ce32ba2ba769b391e2c6122e538a5b3b56
Ganesh Kanchi <ganeshkanchimad@gmail.com>
2022-01-11 23:02:18 +0530

    trimmed the code using ES6

--- index.js
+++ index.js
@@ -4,33 +4,25 @@ const checkButton = document.querySelector("#check");
 const errorDisplay = document.querySelector("#error-display");
 const errorMessage1 = "Total Bill Amount must be greater than 0.";
 const errorMessage2 = "No change needs to be returned.";
-
-const tables = [document.querySelector("#table-1"), document.querySelector("#table-2"), document.querySelector("#table-3"), document.querySelector("#table-4"), document.querySelector("#table-5"), document.querySelector("#table-6"), document.querySelector("#table-7"), document.querySelector("#table-8"), document.querySelector("#table-9")]
-
-
-const notes = ["2000", "500", "100", "20", "10", "5", "1"];
+const tables = document.querySelectorAll(".Tables-output")
+const notes = ["2000", "500","200","100","50","20", "10", "5", "1"];
 
 checkButton.addEventListener("click", function verifyTotalBillAndCashAmount() {
     var returningAmount = (totalCashGiven.value - totalBillAmount.value);
     errorDisplay.style.display = "none";
-    if (totalBillAmount.value > 0) {
-        if (+totalCashGiven.value >= +totalBillAmount.value) {
-            for (i = 0; i < 7; i++) {
+    let currIndex = 0;
 
-                var noOfNotes = (Math.trunc(returningAmount / notes[i]));
-                tables[i].innerText = noOfNotes;
-                returningAmount = (returningAmount) - (noOfNotes * notes[i]);
-            }
-
-        } else {
-            showMessage(errorMessage2)
-        }
-    } else {
-        showMessage(errorMessage1)
-    }
+    (totalBillAmount.value > 0) ? (+totalCashGiven.value >= +totalBillAmount.value) ?
+        notes.map((note,currIndex)=>{
+            var noOfNotes = (Math.trunc(returningAmount / note))
+            tables[currIndex].innerText= noOfNotes;
+            returningAmount = (returningAmount) - (noOfNotes * note)
+            currIndex++;
+        })
+        :showMessage(errorMessage2):showMessage(errorMessage1);
 });
 
-function showMessage(message) {
+const showMessage = message => {
     errorDisplay.style.display = "block";
     errorDisplay.innerText = message;
 }
